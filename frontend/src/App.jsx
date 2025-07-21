15cb79cf8d475703ae14f09f25277e5ca8e77118
Palak.Ved <palak.ved@cognizant.com>
2025-07-21 13:19:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
-import AdminDashboard from './pages/AdminDashboard'
-import DepartmentList from './components/department/DepartmentList'
-import AddDepartment from './components/department/AddDepartment'
+
 function App() {
   const [count, setCount] = useState(0)
 
