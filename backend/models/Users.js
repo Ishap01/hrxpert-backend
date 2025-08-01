15ee8ca9c6a001049a693eb5c7ae79ee9dd0fdcf
palak porwal <porwalpalak10@gmail.com>
2025-08-01 15:14:09 +0530

    new commit

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -4,7 +4,11 @@ const userSchema = new mongoose.Schema({
   name: {type:String, required: true},
   email: { type: String, required: true },
   password: {type:String , required:true},
-  role: { type: String, enum: ['Employee','HR','Admin'], required: 'true' }
+  role: { type: String, enum: ['Employee','HR','Admin'], required: 'true' },
+  profileImage:{type:String},
+  createdAt:{type:Date , default:Date.now},
+  updatedAt:{type:Date , default:Date.now},
+
 });
 
 export default mongoose.model('User', userSchema);
