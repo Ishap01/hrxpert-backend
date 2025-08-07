39e4d6055d83de188e71197006ca0c0549374991
palak porwal <porwalpalak10@gmail.com>
2025-08-07 21:33:12 +0530

    new commit

--- frontend/src/components/employeeDashboard/EmployeeNavbar.jsx
+++ frontend/src/components/employeeDashboard/EmployeeNavbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-//import {useAuth} from '../../context/authContext'
+
 import EmployeeNotifications from './EmployeeNotificationPanel';
 
-const Navbar = ({username}) => {
+const Navbar = () => {
   const navigate = useNavigate();
-  //const {user} = useAuth();
+  
+   const storedUser = localStorage.getItem('user');
+  const user = storedUser ? JSON.parse(storedUser) : null;
+  const username = user?.name || 'user';
   return (
     <div className=' flex items-center text-white justify-between h-12 bg-teal-400 px-5 w-full'>
      
-      <p>Welcome user</p>
+      <p>Welcome {username}</p>
       <div className='ml-auto flex items-center gap-4'>
       <EmployeeNotifications/>
       <button className='px-4 py-1 bg-teal-500 hover:bg-teal-700' onClick={() => navigate("/")}>Logout</button>
