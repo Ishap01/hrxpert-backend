a1373c7f22943bb1d694cd4aa53fa8cb44564d6b
isha <ishaa0182@gmail.com>
2025-09-09 13:35:36 +0530

    updated cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,15 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "https://hrxpert-frontend.vercel.app", // your deployed frontend on Vercel
+    "http://localhost:5173"                // for local testing
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
